677b2c908195e8b777749c59f0136d9b0c2d9f3b
havister <34527383+havister@users.noreply.github.com>
2017-12-20 06:12:44 +0900

    updated index/models.py

--- index/models.py
+++ index/models.py
@@ -5,11 +5,7 @@ from django.db import models
 class Item(models.Model):
     """index item
     """
-<<<<<<< HEAD
     code = models.CharField(max_length=10, unique=True)
-=======
-    code = models.CharField(max_length=6, unique=True)
->>>>>>> 925ff23e104f0b9d3681fd2edcbdffb211034959
     name = models.SlugField(max_length=50)
     market = models.BooleanField(default=False)
     future = models.BooleanField(default=False)
