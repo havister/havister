6e6b09e7a71d6e1fce830956dc7f55fe2be701b2
havister <havister@naver.com>
2017-12-19 17:47:10 +0000

    modified models.py in index app

--- index/models.py
+++ index/models.py
@@ -7,6 +7,9 @@ class Item(models.Model):
     """
     code = models.CharField(max_length=6, primary_key=True)
     name = models.CharField(max_length=50)
+    future = models.BooleanField(default=False)
+    option = models.BooleanField(default=False)
+    fund = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
