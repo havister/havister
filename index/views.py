9424ddc2e96cb3af9c43d9c765a2d0817f03c0b1
havister <havister@naver.com>
2017-12-25 21:31:35 +0000

    fix index/views.py

--- index/views.py
+++ index/views.py
@@ -12,13 +12,13 @@ class ListView(generic.ListView):
     context_object_name = 'index_list'
 
     def get_queryset(self):
-        return Index.objects.order_by('name')
+        return Index.objects.order_by('-market', '-future', '-option', '-fund')
 
 class DetailView(generic.ListView):
     template_name = 'index/detail.html'
     context_object_name = 'index_detail'
 
     def get_queryset(self):
-        self.index = get_object_or_404(Index, slug=self.args[0])
+        self.index = get_object_or_404(Index, slug=self.kwargs['slug'])
         return CalendarMonth.objects.filter(index=self.index)
 
