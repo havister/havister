6949c98316b90a57d29711edb23ed1e6910b9c31
havister <havister@naver.com>
2018-01-10 17:50:40 +0000

    update expiration:models

--- expiration/models.py
+++ expiration/models.py
@@ -5,12 +5,11 @@ from django.db import models
 class Expiration(models.Model):
     """만기"""
     month = models.DateField(primary_key=True)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    open_date = models.DateField()
+    close_date = models.DateField()
 
     def __str__(self):
-        #return "{0}년 {1}월".format(self.month.year, self.month.month)
-        return self.month
+        return "{0}년 {1}월".format(self.month.year, self.month.month)
 
     class Meta:
         db_table = 'expiration'
